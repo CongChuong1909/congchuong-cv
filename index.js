c08fd0db48b0883b2ec2400f8ae9b104f3a0326c
CongChuong <92925332+CongChuong1909@users.noreply.github.com>
2023-04-02 23:42:47 +0700

    update value

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-
-
-
 const loader = document.querySelector("#preloader");
-document.addEventListener("DOMContentLoaded", function(){
-    loader.style.display = 'none';
-})
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    loader.style.display = "none";
+});
 
 ///slick slider
-$(document).ready(function(){
-    $('.testimonials').slick({
+$(document).ready(function () {
+    $(".testimonials").slick({
         dots: true,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         infinite: true,
     });
-  });
+});
 
 // text animation
 const text = document.querySelector(".sec-text");
@@ -31,13 +26,11 @@ const textLoad = () => {
     setTimeout(() => {
         text.textContent = "Programer";
     }, 10000);
-
-}
+};
 textLoad();
 setInterval(textLoad, 15000);
 
 ///sidebar
-
 
 const about = document.querySelector("#about");
 const resume = document.querySelector("#resume");
@@ -49,74 +42,60 @@ const resumeLink = document.querySelector("#resumelink");
 const projectLink = document.querySelector("#projectlink");
 const contactLink = document.querySelector("#contactlink");
 
-const data = [about, resume, project, contact]
-const dataLink = [aboutLink, resumeLink, projectLink, contactLink]
-window.addEventListener('resize', function(event){
+const data = [about, resume, project, contact];
+const dataLink = [aboutLink, resumeLink, projectLink, contactLink];
+window.addEventListener("resize", function (event) {
     handleViewAnimation();
-  });
-  handleViewAnimation();
+});
+handleViewAnimation();
 function handleViewAnimation() {
     var screenWidth = window.innerWidth;
-    if(screenWidth >= 960){
-        dataLink.map((item, index)=>{
-            item.addEventListener('click', function(){
-                data.map((a, i)=>{
-                    if(i !== index)
-                    {
-                        a.classList.add('lg:hidden', 'lg:fadeOutLeft');
-                        a.classList.remove('lg:fadeInLeft');
-                        a.classList.remove('lg:active');
-                        
+    if (screenWidth >= 960) {
+        dataLink.map((item, index) => {
+            item.addEventListener("click", function () {
+                data.map((a, i) => {
+                    if (i !== index) {
+                        a.classList.add("lg:hidden", "lg:fadeOutLeft");
+                        a.classList.remove("lg:fadeInLeft");
+                        a.classList.remove("lg:active");
                     }
-                })
-                data[index].classList.add('lg:active', 'lg:fadeInLeft');
-                data[index].classList.remove('lg:fadeOutLeft');
-                data[index].classList.remove('lg:hidden');
-                
-            })
-        })
+                });
+                data[index].classList.add("lg:active", "lg:fadeInLeft");
+                data[index].classList.remove("lg:fadeOutLeft");
+                data[index].classList.remove("lg:hidden");
+            });
+        });
     }
-    
-  }
-  
+}
 
 // if (screenWidth >= 960) {
-
-
 
 // }
 
 ///popup project
 
-const elementProject = document.querySelectorAll('.overlayx');
+const elementProject = document.querySelectorAll(".overlayx");
 const popup = document.querySelector(".popup_project");
 const close = document.querySelector(".close-btn");
-const nameProject = document.querySelector('.image-name');
+const nameProject = document.querySelector(".image-name");
 const imageProject = document.querySelector(".large-image");
 const layoutProject = document.querySelector(".layout_project");
 for (i = 0; i < elementProject.length; i++) {
-    (function(i) {
-     elementProject[i].addEventListener('click', function() {
-        let name =elementProject[i].getAttribute('name');
-        let image =elementProject[i].getAttribute('img');
-        console.log(name, image);
-        nameProject.innerHTML = name;
-        imageProject.setAttribute('src', image)
-        popup.classList.add('active_project');
-      });
+    (function (i) {
+        elementProject[i].addEventListener("click", function () {
+            let name = elementProject[i].getAttribute("name");
+            let image = elementProject[i].getAttribute("img");
+            console.log(name, image);
+            nameProject.innerHTML = name;
+            imageProject.setAttribute("src", image);
+            popup.classList.add("active_project");
+        });
     })(i);
-  }
-  close.addEventListener('click', function(){
-    popup.classList.remove('active_project')
-  })
-  layoutProject.addEventListener('click', function(e){
+}
+close.addEventListener("click", function () {
+    popup.classList.remove("active_project");
+});
+layoutProject.addEventListener("click", function (e) {
     e.stopPropagation();
-    popup.classList.remove('active_project')
-  })
-
-
-
-
-
-
-
+    popup.classList.remove("active_project");
+});
